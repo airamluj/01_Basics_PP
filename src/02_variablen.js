163c34c8379950e4d96e5e0ee838d2267878b13c
airamluj <jul_maria@hotmail.com>
2025-01-03 09:19:13 +0000

    02

---
+++ src/02_variablen.js
@@ -0,0 +1,15 @@
+
+
+/***** Variablen 01 *******/
+
+// console.log("Hi!");
+// console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName) // Ausgabe
+firstName = "Max"; // Wertzuweisung | assignment
+console.log(firstName) // Ausgabe
+let familyName = "Mutze"; // Deklaration + Wertzuweisung
+console.log("Hi, ich bin " + firstName + " " + familyName + "!" )
